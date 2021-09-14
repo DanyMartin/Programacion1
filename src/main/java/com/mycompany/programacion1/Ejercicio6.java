0d8cfdea6d6c880df21f82a6b21e39183d6ef070
CARLOS MARTIN <CARLOS MARTIN@192.168.0.109>
2021-09-14 11:33:27 -0500

    Cambio de codigo

--- src/main/java/com/mycompany/programacion1/Ejercicio6.java
+++ src/main/java/com/mycompany/programacion1/Ejercicio6.java
@@ -8,7 +8,7 @@ public class Ejercicio6 {
     public static void main(String[] args) {
         float numero1 = 2.3F;
         int numero2 = 5;
-        float división=numero1-numero2;
+        float división=numero1/numero2;
         System.out.println("El resultado es: "+división);
     }
     
